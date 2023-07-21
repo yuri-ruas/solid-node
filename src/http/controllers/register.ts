faf4cd94969d5b23ee609fee42c39848722a774d
Yuri Ruas <yuriruas.dev@gmail.com>
2023-07-21 01:37:23 -0300

    caso de uso e design patterns - handler de erros global

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -27,7 +27,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       return reply.status(409).send({ message: err.message })
     }
 
-    return reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
