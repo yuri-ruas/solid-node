47eff32b3e3f1054fc897e6117e3febf5bdc5c96
Yuri Ruas <yuriruas.dev@gmail.com>
2023-07-22 18:29:06 -0300

    controllers and testes e2e - controller de criação de academia

---
+++ src/http/controllers/gyms/routes.ts
@@ -0,0 +1,6 @@
+import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+export async function gymsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+}
