311e586772a02bc733bed31722ee212fdc4ba576
Yuri Ruas <yuriruas.dev@gmail.com>
2023-07-21 23:22:33 -0300

    implementando casos de uso - caso de uso de academias próximas

---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchNearbyGymsUseCase
+
+describe('Fetch Nearby Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchNearbyGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -22.8554142,
+      longitude: -43.2996352,
+    })
+
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      phone: null,
+      latitude: -22.9508152,
+      longitude: -44.3188465,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -22.8554142,
+      userLongitude: -43.2996352,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
+  })
+})
